be851658004509da28d2dcc1d09bdc34df24d040
Rohit Amarnath <rohit.amarnath@full360.com>
2012-06-07 09:14:26 -0400

    Harvest API now requires spent_at to be just a date, not datetime

--- lib/harvest/expense.rb
+++ lib/harvest/expense.rb
@@ -26,13 +26,8 @@ module Harvest
       super
     end
 
-    def spent_at=(date)
-      self["spent_at"] = (String === date ? Time.parse(date) : date)
-    end
-
     def as_json(args = {})
       super(args).stringify_keys.tap do |hash|
-        hash[json_root].update("spent_at" => (spent_at.nil? ? nil : spent_at.to_time.xmlschema))
         hash[json_root].delete("has_receipt")
         hash[json_root].delete("receipt_url")
       end
